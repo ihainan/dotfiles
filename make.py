5fc53fcd1ae37b0e902da874d8b3220a0d7eb535
ihainan <ihainan72@gmail.com>
2014-09-09 20:21:54 -0700

    Mod : 修改 make.py，删除部分提示信息.

--- make.py
+++ make.py
@@ -10,15 +10,13 @@ def main():
 	home_dir = os.getenv("HOME")
 	bak_dir = home_dir + "/code/dotfiles"	# 备份目录
 	old_dir = home_dir + "/code/old_dotfiles"	# 老文件目录
-	files = "vimrc bashrc tmux zshrc oh-my-zsh"
+	files = "vimrc bashrc tmux.conf zshrc oh-my-zsh"
 
 	# 创建 old_dir 目录
 	print "Removing directory " + old_dir + "."
 	shutil.rmtree(old_dir)
-	print "...Done."
 	print "Creating directory " + old_dir + "."
 	os.mkdir(old_dir)
-	print "...Done."
 
 	# 对于 home 目录下的每一个文件
 	for f in files.split(" "):
@@ -27,13 +25,13 @@ def main():
 			print "Copying " + f + " in home to " + old_dir + "."
 			# print "Move " + home_dir + "/." + f + " " + old_dir
 			shutil.move(home_dir + "/." + f, old_dir)
-			print "...Done."
 
 		# 创建链接到 Home 目录
 		print "Creating Symbol link of " + f + "."
 		os.symlink(bak_dir + "/" + f, home_dir + "/." + f)
 		# print "link -s " + bak_dir + "/" + f + " " + home_dir + "/." + f
-		print "...Done."
+	
+	print ".....Done."
 
 if __name__ == '__main__':
 	main()
