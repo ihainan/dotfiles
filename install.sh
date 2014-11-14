34dd5599ea1f528f35374f1619b9d0976133c365
ihainan <ihainan72@gmail.com>
2014-11-14 10:48:02 +0800

    Add : software install script

---
+++ install.sh
@@ -0,0 +1,13 @@
+# Development Tools
+echo "Installing vim build-essential git curl tmux zsh"
+sudo apt-get install vim build-essential git curl tmux zsh
+
+# oh-my-zsh
+echo "Installing vim build-essential git curl tmux zsh"
+curl -L http://install.ohmyz.sh | sh
+
+# OpenJdk
+sudo apt-get install openjdk-6-jdk
+
+# Pinyin
+sudo apt-get install fcitx fcitx-sunpinyin
